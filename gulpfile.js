201baabaeca23b3b9ae077b6e79aa899252013f7
Travis Maynard <hello@travismaynard.com>
2015-03-30 18:02:06 -0400

    Preparations for punch site.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    myth = require('myth'),
+    myth = require('gulp-myth'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     minify = require('gulp-minify-css'),
@@ -17,11 +17,12 @@ gulp.task('styles', function () {
         .pipe(myth())
         .pipe(minify())
         .pipe(rename('cinch.lib.css'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('site/templates/css'));
 });
 
 gulp.task('watch', function () {
     gulp.watch(css, ['styles']);
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['styles', 'watch']);
