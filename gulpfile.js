370df4b49db0bab2bf15161a5d8cc19e61ea802a
Travis Maynard <hello@travismaynard.com>
2015-03-30 08:25:52 -0400

    Added gulpfile and punch site to project.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    myth = require('myth'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify-css'),
+    ghPages = require('gulp-gh-pages'),
+
+    // Node Modules
+    beep = require('beeper'),
+
+    // Source Files
+    css = ['src/cinch.css'];
+
+gulp.task('styles', function () {
+    gulp.src(css)
+        .pipe(gulp.dest('dist'))
+        .pipe(myth())
+        .pipe(minify())
+        .pipe(rename('cinch.lib.css'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(css, ['styles']);
+});
+
+gulp.task('default', ['styles']);
